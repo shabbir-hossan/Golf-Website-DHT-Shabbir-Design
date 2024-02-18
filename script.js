39348d9d735f9a8b30edd03459e124eab1a468a0
shabbir-hossan <afrankhanshabbir@gmail.com>
2024-02-18 12:44:47 +0600

    js codes added

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ gsap.to('#nav', {
   scrollTrigger: {
     trigger: '#nav',
     scroller: 'body',
-    markers: true,
+    // markers: true,
     start: 'top -10%',
     end: 'top -11%',
     scrub: 1,
@@ -42,9 +42,64 @@ gsap.to('#main', {
   scrollTrigger: {
     trigger: '#main',
     scroller: 'body',
-    markers: true,
+    // markers: true,
     start: 'top -25%',
     end: 'top -70%',
     scrub: 2,
+  },
+});
+
+gsap.from('#about-us img, #about-us-in', {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '#about-us',
+    scroller: 'body',
+    // markers: true,
+    start: 'top 70%',
+    end: 'top 65$',
+    scrub: 2,
+  },
+});
+
+gsap.from('.card', {
+  scale: 0.8,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: '.card',
+    scroller: 'body',
+    // markers: true,
+    start: 'top 60%',
+    end: 'top 65$',
+    scrub: 2,
+  },
+});
+
+gsap.from('.colon1', {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: '.colon1',
+    scroller: 'body',
+    // markers: true,
+    start: 'top 55%',
+    end: 'top 45$',
+    scrub: 4,
+  },
+});
+
+gsap.from('.colon2', {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: '.colon2',
+    scroller: 'body',
+    // markers: true,
+    start: 'top 60%',
+    end: 'top 45$',
+    scrub: 4,
   },
 });
